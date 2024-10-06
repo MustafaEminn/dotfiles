51cfd3ed2b2338b44a4106b418c2bbc51f653dd6
MustafaEminn <mustafaeminbosnali@gmail.com>
2024-10-06 21:28:10 +0300

    changes

--- init.lua
+++ init.lua
@@ -843,7 +843,7 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				tsserver = {},
+				ts_ls = {},
 				eslint = {},
 				eslint_d = {},
 				fixjson = {},
@@ -1068,23 +1068,6 @@ require("lazy").setup({
 			})
 		end,
 	},
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"scottmckendry/cyberdream.nvim",
-		priority = 1001, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("cyberdream")
-
-			-- You can configure highlights by doing something like:
-			-- vim.cmd.hi("Comment gui=none")
-		end,
-	},
 
 	{ -- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then
@@ -1097,10 +1080,10 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			-- vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
-			-- vim.cmd.hi("Comment gui=none")
+			vim.cmd.hi("Comment gui=none")
 		end,
 	},
 
